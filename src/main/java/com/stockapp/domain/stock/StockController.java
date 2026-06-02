6aa66f2bd51aefae93cd41b0063ba11d89196bbf
doltank <doltank777@naver.com>
2026-06-02 21:32:29 +0900

    feat: 종목 최신 현재가 조회 API 추가

--- src/main/java/com/stockapp/domain/stock/StockController.java
+++ src/main/java/com/stockapp/domain/stock/StockController.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class StockController {
 
     private final StockService stockService;
+    private final StockPriceService stockPriceService;
 
     @GetMapping("/search")
     public List<StockResponse> searchStocks(@RequestParam String keyword) {
@@ -20,5 +21,10 @@ public class StockController {
     @GetMapping("/{stockCode}")
     public StockResponse getStock(@PathVariable String stockCode) {
         return stockService.getStock(stockCode);
+    }
+
+    @GetMapping("/{stockCode}/price/latest")
+    public StockPriceResponse getLatestPrice(@PathVariable String stockCode) {
+        return stockPriceService.getLatestPrice(stockCode);
     }
 }
